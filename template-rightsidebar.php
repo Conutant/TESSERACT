8cd053622208a8b133f17de0b69e967ef576dbf4
Gold <andreijonker@mail.com>
2014-12-29 21:55:33 +0800

    Fixing Mobile Nav Issues, Comments part of Right Sidebar and so on

--- template-rightsidebar.php
+++ template-rightsidebar.php
@@ -22,6 +22,12 @@ get_header(); ?>
 					 */
 					get_template_part( 'content', get_post_format() );
 				?>
+				<?php
+					// If comments are open or we have at least one comment, load up the comment template
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
+				?>
 
 			<?php endwhile; ?>
 
