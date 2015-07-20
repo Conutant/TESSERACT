6b81dcfc23f37a5eadd75bf0413aeff7957414cc
danielmarks <daniel@gogoit.com>
2015-07-21 01:38:22 +0530

    Update class-tesseract-plugin-update.php

--- admin/class-tesseract-plugin-update.php
+++ admin/class-tesseract-plugin-update.php
@@ -8,10 +8,35 @@ require_once get_template_directory() . '/admin/class-tesseract-notification.php
 
 final class Tesseract_Plugin_Update{
 	
-	static public function upgrade_info($transient,$pluginslug,$cur_version){
+	
+	static public function check_info($pluginslug,$cur_version,$base_path){
+	
+		$response = Tesseract_Update_Api::doaction(array('action'=>'plugin','slug'=>$pluginslug));
+		
+		if($response == false)	return false;
+ 
+		//check server version against current installed version
+		if( version_compare( $cur_version, $response['new_version'], '<' ) ){
+ 			
+			$obj = new stdClass();
+			
+			foreach($response['upgradeinfo'] as $k =>$v){
+			
+				$obj->$k = $v;
+			
+			}
+			return $obj;
+				
+		}
+		
+	   return false;
+		
+	}
+		
+	static public function upgrade_info($transient,$pluginslug,$cur_version,$base_path){
 		
 		$transient_version = get_transient('tesseract_version_check_'.$pluginslug);
-		
+
 		if(false !== $transient_version){
 		
 			if ( version_compare( $transient_version, $cur_version, '=' ) ) {
@@ -22,18 +47,28 @@ final class Tesseract_Plugin_Update{
 			
 		}	
 		
-		$response = Tesseract_Update_Api::doaction(array('action'=>'plugin','subaction'=>'version'));
-		
+		$response = Tesseract_Update_Api::doaction(array('action'=>'plugin','slug'=>$pluginslug));
+		 
 		if($response == false)	return $transient;
 
 		set_transient('tesseract_version_check_'.$pluginslug, $response['new_version'], ( HOUR_IN_SECONDS * 10 ) );
-
+ 
 		//check server version against current installed version
-		if( version_compare( $transient->checked[$pluginslug], $response['new_version'], '<' ) ){
+		if( version_compare( $cur_version, $response['new_version'], '<' ) ){
 		
-			$transient->response[$pluginslug] = (array) json_decode(json_encode($response['upgradeinfo']));
-				
+		 	$obj = new stdClass();
+			
+			foreach($response['upgradeinfo'] as $k =>$v){
+			
+				$obj->$k = $v;
+			
+			}
+
+			$transient->response[$base_path] = $obj;
+				 
 		}
+		
+ 
 		
 	   return $transient;
 		
