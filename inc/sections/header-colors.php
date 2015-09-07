f5ec49238eaa72368d795ddf989244afb61b70ec
Andy Adams <andyadamscp@gmail.com>
2015-09-07 13:59:17 -0700

    Removing front_page restriction on opacity controls

--- inc/sections/header-colors.php
+++ inc/sections/header-colors.php
@@ -1,42 +1,42 @@
 <?php
-/*  
+/*
  * section HEADER COLORS
- */		
-	
+ */
+
    	$wp_customize->add_section( 'tesseract_header_colors' , array(
     	'title'      => __('Header Colors', 'tesseract'),
     	'priority'   => 1,
 		'panel'      => 'tesseract_header_options'
-	) );	
+	) );
 
 		//Register setting with the custom ALPHA enabled colorpicker
 		// See full blog post here
-		// http://pluto.kiwi.nz/2014/07/how-to-add-a-color-control-with-alphaopacity-to-the-wordpress-theme-customizer/		
-	
+		// http://pluto.kiwi.nz/2014/07/how-to-add-a-color-control-with-alphaopacity-to-the-wordpress-theme-customizer/
+
 		$wp_customize->add_setting( 'tesseract_header_colors_bck_color', array(
 				'transport'         => 'postMessage',
 				'sanitize_callback' => 'tesseract_sanitize_rgba',
 				'default' 			=> '#59bcd9'
 		) );
 
-			$wp_customize->add_control( 
-				new WP_Customize_Color_Control( 
-				$wp_customize, 
-				'tesseract_header_colors_bck_color_control', 
+			$wp_customize->add_control(
+				new WP_Customize_Color_Control(
+				$wp_customize,
+				'tesseract_header_colors_bck_color_control',
 				array(
 					'label'      => __( 'Header Background Color', 'tesseract' ),
 					'section'    => 'tesseract_header_colors',
 					'settings'   => 'tesseract_header_colors_bck_color',
 					'priority'   => 1
-				) ) 						
-			);	
-			
+				) )
+			);
+
 		$wp_customize->add_setting( 'tesseract_header_colors_bck_color_opacity', array(
 				'transport'         => 'postMessage',
 				'sanitize_callback' => 'esc_html',
 				'default' 			=> 100
-		) );			
-			
+		) );
+
 			$wp_customize->add_control( 'tesseract_header_colors_bck_color_opacity_control', array(
 				'type'        => 'range',
 				'priority'    => 2,
@@ -50,60 +50,59 @@
 					'step'  => 5,
 					'class' => 'tesseract-tho-header-colors-bck-opacity',
 					'style' => 'color: #0a0',
-				),
-				'active_callback' => 'is_front_page'
-			) );			
-			
+				)
+			) );
+
 		$wp_customize->add_setting( 'tesseract_header_colors_text_color', array(
 				'transport'         => 'postMessage',
 				'sanitize_callback' => 'sanitize_hex_color',
 				'default' 			=> '#ffffff'
 		) );
-	
-			$wp_customize->add_control( 
-				new WP_Customize_Color_Control( 
-				$wp_customize, 
-				'tesseract_header_colors_text_color_control', 
+
+			$wp_customize->add_control(
+				new WP_Customize_Color_Control(
+				$wp_customize,
+				'tesseract_header_colors_text_color_control',
 				array(
 					'label'      => __( 'Header Text Color', 'tesseract' ),
 					'section'    => 'tesseract_header_colors',
 					'settings'   => 'tesseract_header_colors_text_color',
 					'priority'   => 2
-				) ) 						
-			);	
-			
+				) )
+			);
+
 		$wp_customize->add_setting( 'tesseract_header_colors_link_color', array(
 				'transport'         => 'postMessage',
 				'sanitize_callback' => 'sanitize_hex_color',
 				'default' 			=> '#ffffff'
 		) );
-	
-			$wp_customize->add_control( 
-				new WP_Customize_Color_Control( 
-				$wp_customize, 
-				'tesseract_header_colors_link_color_control', 
+
+			$wp_customize->add_control(
+				new WP_Customize_Color_Control(
+				$wp_customize,
+				'tesseract_header_colors_link_color_control',
 				array(
 					'label'      => __( 'Header Link Color', 'tesseract' ),
 					'section'    => 'tesseract_header_colors',
 					'settings'   => 'tesseract_header_colors_link_color',
 					'priority' 	 => 3
-				) ) 						
+				) )
 			);
-			
+
 		$wp_customize->add_setting( 'tesseract_header_colors_link_hover_color', array(
 				'transport'         => 'postMessage',
 				'sanitize_callback' => 'sanitize_hex_color',
 				'default' 			=> '#d1ecff'
 		) );
-	
-			$wp_customize->add_control( 
-				new WP_Customize_Color_Control( 
-				$wp_customize, 
-				'tesseract_header_colors_link_hover_color_control', 
+
+			$wp_customize->add_control(
+				new WP_Customize_Color_Control(
+				$wp_customize,
+				'tesseract_header_colors_link_hover_color_control',
 				array(
 					'label'      => __( 'Header Hovered Link Color', 'tesseract' ),
 					'section'    => 'tesseract_header_colors',
 					'settings'   => 'tesseract_header_colors_link_hover_color',
 					'priority'   => 4
-				) ) 						
+				) )
 			);
