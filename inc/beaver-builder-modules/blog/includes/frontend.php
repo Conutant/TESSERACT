f977ecb9e97d4f766c41cd83d3330a185774e908
Andy Adams <andyadamscp@gmail.com>
2015-09-08 16:42:12 -0700

    Adding initial frontend look to blog posts

--- inc/beaver-builder-modules/blog/includes/frontend.php
+++ inc/beaver-builder-modules/blog/includes/frontend.php
@@ -1,1 +1,20 @@
-<h1>This is your blog</h1>
+<?php
+
+// Get the query data. NULL because we have no settings yet.
+$query = FLBuilderLoop::query( NULL );
+
+?>
+<?php if( $query->have_posts() ) : ?>
+	<div class="tesseract-post-list">
+		<?php while( $query->have_posts() ) : $query->the_post(); ?>
+			<?php include $module->dir . 'includes/post-list.php'; ?>
+		<?php endwhile; ?>
+	</div>
+	<div class="tesseract-pagination">
+		<?php FLBuilderLoop::pagination( $query ); ?>
+	</div>
+<?php endif; ?>
+<?php if( ! $query->have_posts() && ( defined('DOING_AJAX') || isset($_REQUEST['fl_builder'] ) ) ) : ?>
+	<div class="fl-post-grid-empty"><?php _e( 'No posts found.', 'fl-builder' ); ?></div>
+<?php endif; ?>
+<?php wp_reset_postdata(); ?>
---
+++ inc/beaver-builder-modules/blog/includes/post-list.php
@@ -0,0 +1,27 @@
+<div class="tesseract-post">
+	<div class="post-header">
+		<a href="<?php the_permalink(); ?>">
+			<?php if ( has_post_thumbnail() ) : ?>
+				<div class="featured-image">
+					<?php the_post_thumbnail( 'medium' ); ?>
+				</div>
+			<?php endif; ?>
+			<div class="date">
+				<?php the_time( get_option( 'date_format' ) ); ?>
+			</div>
+			<div class="author">
+				<?php the_author(); ?>
+			</div>
+		</a>
+	</div>
+	<div class="entry">
+		<h2 class="title">
+			<a href="<?php the_permalink(); ?>">
+				<?php the_title(); ?>
+			</a>
+		</h2>
+		<div class="content">
+			<?php the_excerpt(); ?>
+		</div>
+	</div>
+</div>
