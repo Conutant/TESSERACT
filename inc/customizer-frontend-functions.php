1baa62f47a511db5201a9923a4d8daa6ca8c91d1
Andy Adams <andyadamscp@gmail.com>
2015-09-22 14:01:51 -0700

    Fixing social network display on V2

--- inc/customizer-frontend-functions.php
+++ inc/customizer-frontend-functions.php
@@ -1,151 +1,123 @@
-<?php 
+<?php
 
 function tesseract_header_right_content( $content ) {
-	
+
 	switch( $content ) {
 
 		// Step 1 -> nothing
-		default:						
-			break;						
-		
+		default:
+			break;
+
 		// Step 2 -> logo
-		case 'buttons': 
-			
+		case 'buttons':
+
 			$code = get_theme_mod('tesseract_header_content_if_button');
 			if ( ( get_theme_mod('tesseract_header_right_content') == 'buttons' ) && ( !$code || !isset($code) ) ) {
-				echo '<div id="header-button-container"><div id="header-button-container-inner"><a href="/" class="button primary-button">Primary Button</a><a href="/" class="button secondary-button">Secondary Button</a></div></div>';	
+				echo '<div id="header-button-container"><div id="header-button-container-inner"><a href="/" class="button primary-button">Primary Button</a><a href="/" class="button secondary-button">Secondary Button</a></div></div>';
 			} else {
 				echo '<div id="header-button-container"><div id="header-button-container-inner">' . do_shortcode( $code ) . '</div></div>';
 			}
-			
-			break; 
-			
+
+			break;
+
 		// Step 3 -> social
 		case 'social': ?>
-			
-            <div class="social-wrapper cf">	
-				<ul class="hr-social">
-                	<?php $sn = array(
-						'fb' => __( 'Facebook', 'tesseract' ),
-						'tw' => __( 'Twitter', 'tesseract' ),
-						'gplus' => __( 'Google Plus', 'tesseract' ),
-						'li' => __( 'LinkedIn', 'tesseract' ),
-						'yt' => __( 'YouTube', 'tesseract' ),
-						'vim' => __( 'Vimeo', 'tesseract' ),
-						'tumb' => __( 'Tumblr', 'tesseract' ),
-						'fr' => __( 'FlickR', 'tesseract' ),
-						'pin' => __( 'Pinterest', 'tesseract' ),
-						'dr' => __( 'Dribbble', 'tesseract' )
-					);
-					
-					foreach ( $sn as $sn_short => $sn_full ) {
-						
-						$sn_img = get_theme_mod('tesseract_' . $sn_short . '_image');
-						$sn_url = get_theme_mod('tesseract_' . $sn_short . '_url');						
-						
-						if ( $sn_img && $sn_url ):
-							echo '<li><a title="' . __( 'Follow Us on ', 'tesseract' ) . $sn_full . '" href="' . $sn_url . '" target="_blank"><img src="' . $sn_img . '" width="24" height="24" alt="' . $sn_full . ' icon" /></a></li>';
-						endif;
-						
-					} ?>
 
+            <div class="social-wrapper cf">
+				<ul class="hr-social">
+					<?php tesseract_display_social_network_list_items(); ?>
 				</ul>
 			</div>
-            
+
 		<?php break;
-			
+
 		// Step 5 -> search
 		case 'search':
-		
+
 			get_search_form();
-		
-			break;	
-			
-		// Step 6 -> menu	
+
+			break;
+
+		// Step 6 -> menu
 		case 'menu'; ?>
-		
+
           	<nav id="header-right-menu" role="navigation">
 				<?php tesseract_output_menu( FALSE, FALSE, 'primary_right', 0 ); ?>
-			</nav>						
-		
+			</nav>
+
 	<?php }
-	
+
 }
 
-
 function tesseract_horizontal_footer_menu_additional_content( $content ) {
-	
+
 	$headerLogo = get_theme_mod('tesseract_header_logo_image');
 	$footerLogo = get_theme_mod('tesseract_footer_logo_image');
 	$footerLogoEnable = ( get_theme_mod('tesseract_footer_logo_enable') == 1 ) ? true : false;
-	
+
 	switch( $content ) {
 
 		// Step 1 -> nothing
-		default:						
-			break;						
-		
+		default:
+			break;
+
 		// Step 2 -> logo
-		case 'logo': 
-			
+		case 'logo':
+
 			$logoImg = ( $footerLogoEnable && $footerLogo ) ? $footerLogo : $headerLogo;
-			
+
 			if ( $logoImg ) : ?>
-			
-				<div class="site-branding">								
+
+				<div class="site-branding">
 					<h1 class="site-logo"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img src="<?php echo $logoImg; ?>" alt="logo" /></a></h1>
 				</div>
-				
+
 			<?php else : ?>
-			
-				<div class="site-branding">								
+
+				<div class="site-branding">
 					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php echo get_bloginfo('blogname'); ?></a></h1>
-				</div>            
-            
-			<?php endif; 						
-			break; 
-			
+				</div>
+
+			<?php endif;
+			break;
+
 		// Step 3 -> social
 		case 'social': ?>
-		
-				<ul class="hm-social">
-                	<?php $sn = array(
-						'fb' => __( 'Facebook', 'tesseract' ),
-						'tw' => __( 'Twitter', 'tesseract' ),
-						'gplus' => __( 'Google Plus', 'tesseract' ),
-						'li' => __( 'LinkedIn', 'tesseract' ),
-						'yt' => __( 'YouTube', 'tesseract' ),
-						'vim' => __( 'Vimeo', 'tesseract' ),
-						'tumb' => __( 'Tumblr', 'tesseract' ),
-						'fr' => __( 'FlickR', 'tesseract' ),
-						'pin' => __( 'Pinterest', 'tesseract' ),
-						'dr' => __( 'Dribbble', 'tesseract' )
-					);
-					
-					foreach ( $sn as $sn_short => $sn_full ) {
-						
-						$sn_img = get_theme_mod('tesseract_' . $sn_short . '_image');
-						$sn_url = get_theme_mod('tesseract_' . $sn_short . '_url');						
-						
-						if ( $sn_img && $sn_url ):
-							echo '<li><a title="Follow Us on ' . $sn_full . '" href="' . $sn_url . '" target="_blank"><img src="' . $sn_img . '" width="24" height="24" alt="' . $sn_full . ' icon" /></a></li>';
-						endif;
-						
-					} ?>
 
+				<ul class="hm-social">
+					<?php tesseract_display_social_network_list_items(); ?>
 				</ul>
-		
+
 			<?php break;
-			
+
 		// Step 5 -> search
 		case 'search':
-		
+
 			get_search_form();
-		
-			break;						
-		
+
+			break;
+
 	}
-	
+
+}
+
+/**
+ * Helper function to display social networks as a series of list items.
+ */
+function tesseract_display_social_network_list_items() {
+	for ( $i = 1; $i <= 10; $i++ ) {
+		$sn_img = get_theme_mod( 'tesseract_social_account0' . $i . '_image' );
+
+		// Quit early if no image is found.
+		if ( $sn_img ) {
+			$sn_name = get_theme_mod( 'tesseract_social_account0' . $i . '_name' );
+			$sn_url = get_theme_mod( 'tesseract_social_account0' . $i . '_url' );
+
+			if ( $sn_name && $sn_url ) {
+				echo '<li><a title="Follow Us on ' . $sn_name . '" href="' . $sn_url . '" target="_blank"><img src="' . $sn_img . '" width="24" height="24" alt="' . $sn_full . ' icon" /></a></li>';
+			}
+		}
+	}
 }
 
 /**
@@ -153,7 +125,7 @@ function tesseract_horizontal_footer_menu_additional_content( $content ) {
  * source at http://css-tricks.com/snippets/php/convert-hex-to-rgb/
  *
  */
-   
+
 function tesseract_hex2rgb( $colour ) {
         if ( $colour[0] == '#' ) {
                 $colour = substr( $colour, 1 );
