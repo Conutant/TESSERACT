af4aa77f9f0f199d6cd90e0f78312bf756316086
Andres Olvera <aolvera@andrux.net>
2015-10-04 14:01:33 -0500

    removed all references to the static map type

--- inc/beaver-builder-modules/google-map/includes/frontend.css.php
+++ inc/beaver-builder-modules/google-map/includes/frontend.css.php
@@ -8,16 +8,8 @@
 }
 
 .fl-node-<?php echo $id; ?> {
-	<?php if ( $settings->map_type === 'static' ): ?>
-	width: <?php echo $settings->width; ?>px;
-	<?php endif; ?>
 	max-width: 100%;
-	<?php if ( $settings->fullwidth === 'no' || $settings->map_type === 'static' ): ?>
+	<?php if ( $settings->fullwidth === 'no' ): ?>
 	float: <?php echo $settings->float; ?>;
 	<?php endif; ?>
-}
-
-.fl-node-<?php echo $id; ?> .fl-module-content .map-wrapper img {
-	width: 100%;
-	max-width: 100%;
 }
